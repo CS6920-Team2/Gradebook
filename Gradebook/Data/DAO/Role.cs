04d39b1f240a1fce5543b756398439e80b277220
[jeradcliffe] <jacobradcliffe2012@gmail.com>
2017-06-23 15:01:57 -0500

    Role Services added.

---
+++ Gradebook/Data/DAO/Role.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook.Data.DAO
+{
+    class Role
+    {
+        public int roleID { get; set; }
+        public string role { get; set; }
+}
+}
---
+++ Gradebook/Data/Services/RoleService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using Gradebook.Data.DAO;
+using Gradebook.Data.Factories;
+using Gradebook.Data.Interfaces;
+
+namespace Gradebook.Data.Services
+{
+    class RoleService : IRoleService
+    {
+        public string findRole(int roleID)
+        {
+            Role roleObject;
+            using (var connection = ConnectionFactory.GetOpenConnection())
+            {
+                roleObject = connection.Query<Role>("select role from Roles where roleID = @roleID", new {roleID = roleID}).FirstOrDefault();
+            }
+            return roleObject.role;
+        }
+    }
+}
