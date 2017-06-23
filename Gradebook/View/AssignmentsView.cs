d9564e4e564c2892123be3c03b8ed5a1cf6b046f
Phillip Thomas <pthomas5@my.westga.edu>
2017-06-22 22:37:47 -0400

    Created an AssignmentsView form

---
+++ Gradebook/View/AssignmentsView.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace Gradebook
+{
+    internal class AssignmentsView : Form
+    {
+        public MainMDI MdiParent { get; set; }
+
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // AssignmentsView
+            // 
+            this.ClientSize = new System.Drawing.Size(356, 338);
+            this.Name = "AssignmentsView";
+            this.Text = "Assignments View";
+            this.ResumeLayout(false);
+
+        }
+    }
+}
