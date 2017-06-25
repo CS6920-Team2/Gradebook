21cc20347d991b65b3a41f4771e9be6d922f63cc
Phillip Thomas <pthomas5@my.westga.edu>
2017-06-25 12:12:57 -0400

    Added Validator class to help with validate controls.

---
+++ Gradebook/Data/Utils/Validator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Gradebook.Data.Utils
+{
+    public class Validator
+    {
+
+        private static string title = "Entry Error";
+
+        // Returns title of the message box
+        public static string Title
+        {
+            get
+            {
+                return title;
+            }
+            set
+            {
+                title = value;
+            }
+        }
+
+        // Checks that the information is present
+        public static bool IsPresent(Control control)
+        {
+            if (control.GetType().ToString() == "System.Windows.Forms.TextBox")
+            {
+                TextBox textBox = (TextBox)control;
+                if (string.IsNullOrWhiteSpace(textBox.Text))
+                {
+                    MessageBox.Show(textBox.Tag.ToString() + " is a required field.", Title);
+                    textBox.Focus();
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            else if (control.GetType().ToString() == "System.Windows.Forms.ComboBox")
+            {
+                ComboBox comboBox = (ComboBox)control;
+                if (comboBox.SelectedIndex == -1)
+                {
+                    MessageBox.Show(comboBox.Tag.ToString() + " is a required field.", Title);
+                    comboBox.Focus();
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            return true;
+        }
+    }
+}
