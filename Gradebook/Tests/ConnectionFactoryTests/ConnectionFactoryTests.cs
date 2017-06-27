d270c63f23c611c8378eb7ee21a05b75a7623d08
[jeradcliffe] <jacobradcliffe2012@gmail.com>
2017-06-27 12:47:05 -0500

    Adjusted Testing Suite: Now hosted in Gradebook project to allow connection to DB.

---
+++ Gradebook/Tests/ConnectionFactoryTests/ConnectionFactoryTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gradebook.Data.Factories;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace Gradebook.Tests.ConnectionFactoryTests
+{
+    [TestClass]
+    public class ConnectionFactoryTests
+    {
+        [TestMethod]
+        public void GetOpenConnectionTest()
+        {
+            using (var connection = ConnectionFactory.GetOpenConnection())
+            {
+                Assert.IsNotNull(connection);
+                Assert.AreEqual(connection.State, ConnectionState.Open);
+            }
+        }
+
+        [TestMethod]
+        public void GetOpenSQLiteConnectionTest()
+        {
+            using (var connection = ConnectionFactory.GetOpenSQLiteConnection())
+            {
+                Assert.IsNotNull(connection);
+                Assert.AreEqual(connection.State, ConnectionState.Open);
+            }
+        }
+    }
+}
---
+++ Gradebook/Tests/ServiceTests/AdminServiceTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gradebook.Data.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gradebook.Data.DAO;
+
+namespace Gradebook.GradebookTests.ServiceTests
+{
+    [TestClass]
+    public class AdminServiceTests
+    {
+        private AdminService adminService;
+        private Admin admin;
+
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            adminService = new AdminService();
+        }
+
+        [TestMethod]
+        public void getAdminByUserIDTestSuccess()
+        {
+            admin = adminService.getAdminByUserID(11);
+            Assert.IsNotNull(admin);
+            Assert.AreEqual(1, admin.adminID);
+            Assert.AreEqual(11, admin.personID);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void getAdminByUserIDReturnNull()
+        {
+            admin = adminService.getAdminByUserID(1);
+        }
+    }
+}
