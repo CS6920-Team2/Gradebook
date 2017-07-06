35d2cf0772d94aaf97334e44aea57d6985c285b0
[jeradcliffe] <jacobradcliffe2012@gmail.com>
2017-07-06 17:40:18 -0500

    #28 Registered Student DAO class created

---
+++ Gradebook/Data/DAO/RegisteredStudent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook.Data.DAO
+{
+    public class RegisteredStudent : Student
+    {
+        public int registeredStudentID { get; set; }
+        public int taughtCourseID { get; set; }
+    }
+}
