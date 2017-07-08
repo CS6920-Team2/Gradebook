20860dcd32443e2abd9e2ded4403cf5891d01e9c
Phillip Thomas <pthomas5@my.westga.edu>
2017-07-07 22:08:56 -0400

    Created interface for the ReportService

---
+++ Gradebook/Data/Interfaces/IReportService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gradebook.Data.Interfaces
+{
+    interface IReportService
+    {
+
+        DataSet CreateProgressReportDataSet(int taughtCourseID, int studentID);
+    }
+}
