cb90e9b45133226e7feacc1996d0a232eac608cc
Phillip Thomas <pthomas5@my.westga.edu>
2017-07-08 01:37:39 -0400

    ProgressReport now gives average grade and letter grade.

--- Gradebook/View/ProgressReportView.Designer.cs
+++ Gradebook/View/ProgressReportView.Designer.cs
@@ -29,6 +29,8 @@
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.gradeAvgLBL = new System.Windows.Forms.Label();
+            this.letterGradeLBL = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -38,29 +40,54 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Window;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(86, 80);
+            this.dataGridView1.Location = new System.Drawing.Point(104, 183);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersVisible = false;
             this.dataGridView1.Size = new System.Drawing.Size(611, 280);
             this.dataGridView1.TabIndex = 0;
             // 
+            // gradeAvgLBL
+            // 
+            this.gradeAvgLBL.AutoSize = true;
+            this.gradeAvgLBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gradeAvgLBL.Location = new System.Drawing.Point(107, 149);
+            this.gradeAvgLBL.Name = "gradeAvgLBL";
+            this.gradeAvgLBL.Size = new System.Drawing.Size(197, 31);
+            this.gradeAvgLBL.TabIndex = 1;
+            this.gradeAvgLBL.Text = "Grade Average";
+            // 
+            // letterGradeLBL
+            // 
+            this.letterGradeLBL.AutoSize = true;
+            this.letterGradeLBL.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.letterGradeLBL.Location = new System.Drawing.Point(476, 149);
+            this.letterGradeLBL.Name = "letterGradeLBL";
+            this.letterGradeLBL.Size = new System.Drawing.Size(166, 31);
+            this.letterGradeLBL.TabIndex = 2;
+            this.letterGradeLBL.Text = "Letter Grade";
+            // 
             // ProgressReportView
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(792, 372);
+            this.ClientSize = new System.Drawing.Size(836, 488);
+            this.Controls.Add(this.letterGradeLBL);
+            this.Controls.Add(this.gradeAvgLBL);
             this.Controls.Add(this.dataGridView1);
             this.Name = "ProgressReportView";
             this.Text = "Progress Report View";
             this.Load += new System.EventHandler(this.ProgressReportView_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label gradeAvgLBL;
+        private System.Windows.Forms.Label letterGradeLBL;
     }
 }
--- Gradebook/View/ProgressReportView.cs
+++ Gradebook/View/ProgressReportView.cs
@@ -34,6 +34,8 @@ namespace Gradebook.View
             dataGridView1.Columns["personID"].Visible = false;
             dataGridView1.Columns["FirstName"].Visible = false;
             dataGridView1.Columns["LastName"].Visible = false;
+            gradeAvgLBL.Text = "Grade Average: " + getFinalGrade() +"%";
+            letterGradeLBL.Text = "Letter Grade: " + getLetterGrade(getFinalGrade());
         }
 
         // Loads the datagridview .
@@ -41,6 +43,83 @@ namespace Gradebook.View
         {
             ds = reportService.CreateProgressReportDataSet(sID,tcID);
             dataGridView1.DataSource = ds.Tables[0];
+        }
+
+        private decimal getFinalGrade()
+        {
+            decimal actualPoints = 0;
+            decimal possiblePoints = 0;
+            int weight = 0;
+            decimal pointAvg = 0;
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                weight = Int32.Parse(row.Cells[7].Value.ToString());
+                decimal weightPercentage = (decimal)weight / 100;
+                actualPoints += (decimal)Int32.Parse(row.Cells[5].Value.ToString()) * weightPercentage;
+                possiblePoints += (decimal)Int32.Parse(row.Cells[6].Value.ToString()) * weightPercentage;
+                
+            }
+            pointAvg = (actualPoints / possiblePoints) * 100;
+            pointAvg = Math.Round(pointAvg, 2);
+            return pointAvg;
+        }
+
+        private string getLetterGrade(decimal gradeAvg)
+        {
+            string letterGrade = "";
+            if(gradeAvg >=97)
+            {
+                letterGrade = "A+";
+            }
+            else if (gradeAvg >= 93 && gradeAvg < 97)
+            {
+                letterGrade = "A";
+            }
+            else if (gradeAvg >= 90 && gradeAvg < 93)
+            {
+                letterGrade = "A-";
+            }
+            else if (gradeAvg >= 87 && gradeAvg < 90)
+            {
+                letterGrade = "B+";
+            }
+            else if (gradeAvg >= 83 && gradeAvg < 87)
+            {
+                letterGrade = "B";
+            }
+            else if (gradeAvg >= 80 && gradeAvg < 83)
+            {
+                letterGrade = "B-";
+            }
+            else if (gradeAvg >= 77 && gradeAvg < 80)
+            {
+                letterGrade = "C+";
+            }
+            else if (gradeAvg >= 73 && gradeAvg < 77)
+            {
+                letterGrade = "C";
+            }
+            else if (gradeAvg >= 70 && gradeAvg < 73)
+            {
+                letterGrade = "C-";
+            }
+            else if (gradeAvg >= 67 && gradeAvg < 70)
+            {
+                letterGrade = "D+";
+            }
+            else if (gradeAvg >= 63 && gradeAvg < 67)
+            {
+                letterGrade = "D";
+            }
+            else if (gradeAvg >= 60 && gradeAvg < 63)
+            {
+                letterGrade = "D-";
+            }
+            else
+            {
+                letterGrade = "F";
+            }
+            return letterGrade;
         }
     }
 }
